3d00e9137456ff6a3cb96ba7e279286b0142b8d3
Philippe Guilbault <pguilbault@funio.com>
2012-04-17 16:20:39 -0400

    Cleaned up the structure and added Basic Auth event

--- index.js
+++ index.js
@@ -1,130 +1,135 @@
 var fs = require('fs')
   , path = require('path')
-  , cache = {
-      idle_threshold: 300,
-      ratio_threshold: 10,
-      timeouts: {}
-  }
-  , Config = require('Config')
-  , parseText = require('shared/TextParser').parse
-  , files = {};
+  , util = require('util')
+  , mime = require('mime')
+  , Config = require('NoobConfig')
+;
 
-module.exports.processRequest = function processRequest(req, res, basePath) {
-    var basePath = (basePath === undefined ? process.cwd() + '/public/' : basePath)
-      , reqUrl = req.url;
+
+function NoobHTTP (config) {
+    var self = this
+      , now = new Date()
+      , date = now.getFullYear()+'.'
+          +(now.getMonth() < 10 ? '0'+now.getMonth() : now.getMonth())+'.'
+          +(now.getDate() < 10 ? '0'+now.getDate() : now.getDate())
+    this.logFile = fs.createWriteStream(Config('NoobHTTP').path.logs + config.name +'.'+ date +'.log', {'flags': 'a'});
     
-    var filename = basePath +'index.html';
-    if ('/' != reqUrl) {
-        filename = basePath + reqUrl;
-    }
-    filename = path.normalize(filename);
-  
-    if (!files.hasOwnProperty(filename)) {
-        files[ filename ] = {
-            data: null,
-            ratio: 0,
-            lastCacheAccess: null
-        };
-    }
-
-    var mimeType = 'text/plain';
-    switch (path.extname(filename)) {
-        case '.html':
-            mimeType = 'text/html';
-            break;
-        case '.wav':
-            mimeType = 'audio/wav';
-            break;
-        case '.mp3':
-            mimeType = 'audio/mpeg';
-            break;
-        case '.ogg':
-            mimeType = 'audio/ogg';
-            break;
-        case '.css':
-            mimeType = 'text/css';
-            break;
-        case '.png':
-            mimeType = 'image/png';
-            break;
-        case '.js':
-            mimeType = 'application/x-javascript';
-            break;
-        case '.ini':
-            mimeType = 'application/octet-stream';
-            break;
-    }
-
-    if (files[ filename ]['data'] != null) {
-        res.writeHead(200, {'Content-Type': mimeType});
-        res.end(files[ filename ]['data']);
-
-        files[ filename ]['lastCacheAccess'] = new Date();
+    this.auth = config.auth;
+    this.name = config.name;
+    this.path = config.path;
+    this.isSSL = config.isSSL;
+    if (config.isSSL) {
+        this.http_server = require('https').createServer({
+            key: fs.readFileSync(Config('NoobHTTP').path.ssl+'privatekey.pem'),
+            cert: fs.readFileSync(Config('NoobHTTP').path.ssl+'certificate.pem')
+        }, function (req, res) {
+            self.processRequest.call(self, req, res);
+        });
     }
     else {
+        this.http_server = require('http').createServer(function (req, res) {
+            self.processRequest.call(self, req, res);
+        });
+    }
 
-        function response(index, filename) {
-            fs.readFile(filename, function (err, data) {
-                if (err) {
-                    res.writeHead(500, {'Content-Type': 'text/html'});
-                    res.end(fs.readFileSync('public/errors/500.html'));
+    this.http_server.listen(config.port);
+};
+util.inherits(NoobHTTP, require('events').EventEmitter);
+
+NoobHTTP.prototype.response = function response(filename, res, log) {
+    var self = this;
+
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            log.error = err;
+            log.code = 500;
+            self.logFile.write(JSON.stringify(log)+"\n");
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            res.end(fs.readFileSync('public/errors/500.html'));
+            return;
+        }
+
+        log.code = 200;
+        self.logFile.write(JSON.stringify(log)+"\n");
+        res.writeHead(200, {'Content-Type': mime.lookup(filename)});
+        res.end(data);
+    });
+};
+
+NoobHTTP.prototype.requestBasicAuth = function requestBasicAuth(res, log) {
+    log.code = 401;
+    this.logFile.write(JSON.stringify(log)+"\n");
+    res.writeHead(401, {
+        'Content-Type': 'text/plain',
+        'WWW-Authenticate': 'Basic realm="NoobHTTP"'
+    });
+    res.end('Authentication required');
+};
+
+NoobHTTP.prototype.processRequest = function processRequest(req, res) {
+    var self = this
+      , filename = this.path +'index.html';
+
+    if ('/' != req.url) {
+        filename = this.path + req.url;
+    }
+    filename = path.normalize(filename);
+
+    var log = getLog(req);
+    if (path.existsSync(filename)) {
+        if (this.auth) {
+            if (!req.headers.hasOwnProperty('authorization')) {
+                this.requestBasicAuth(res, log);
+                return;
+            }
+
+            var header=req.headers['authorization']||'', token=header.split(/\s+/).pop()||'', auth=new Buffer(token, 'base64').toString(), parts=auth.split(/:/)
+              , username=parts[0]
+              , password=parts[1];
+            this.emit('authenticate', username, password, function (isAuthenticated) {
+                if (isAuthenticated) {
+                    self.response(filename, res, log);
                 }
-                
-                // if its require.js we need to parse the configs that starts the whole thing rolling
-                if (filename.match(/require\.js$/)) {
-                    data = parseText(Config('settings').NoobDesk, data.toString(), '{_', '_}');
+                else {
+                    self.requestBasicAuth(res, log);
                 }
-                
-                res.writeHead(200, {'Content-Type': mimeType});
-                res.end(data);
-
-                if ((!cache['timeouts'].hasOwnProperty(index) || cache['timeouts'][ index ] == null) && files[ index ]['data'] == null) {
-                    cache['timeouts'][ index ] = setTimeout(function (index, data) {
-                        //  if the file exceeds the ratio threshold, we put it in memory
-                        if (files[ index ]['ratio'] > cache['ratio_threshold']) {
-                            files[ index ]['data'] = data;
-                        }
-
-                        // Reset the ratio every time
-                        files[ index ]['ratio'] = 0;
-                        clearTimeout(cache['timeouts'][ index ]);
-                        cache['timeouts'][ index ] = null;
-                    }, 1000, index, data);
-                }
-
-                files[ index ]['ratio']++;
             });
         }
-
-        if (path.existsSync(filename)) {
-            response(filename, filename);
-        }
-        // If its a js and is shared we return it
-        else if ('js' == path.extname(filename) && path.existsSync(filename)) {
-            response(filename, filename);
-        }
         else {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.end(fs.readFileSync('public/errors/404.html'));
+            this.response(filename, res, log);
         }
     }
+    else {
+        log.code = 404;
+        self.logFile.write(JSON.stringify(log)+"\n");
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Error 404');
+    }
+
+};
+
+function getLog(req) {
+    return {
+        ip: req.socket.remoteAddress,
+        date: new Date(),
+        pid: process.pid,
+        method: req.method,
+        url: req.url,
+        referer: req.headers['referer'],
+        "user-agent": req.headers['user-agent']
+    };
 }
 
-setInterval(cleanHttpCache, 600000);
-function cleanHttpCache() {
-    var date = new Date();
-    for (var filename in files) {
-        if (files[ filename ]['lastCacheAccess'] != null) {
-            var idleTime = (date.getTime() - files[ filename ]['lastCacheAccess'].getTime()) / 1000;
-            // if the cached file as exceeded its idle threshold we take it off the cache
-            if (idleTime >= cache['idle_threshold']) {
-                files[ filename ] = {
-                    data: null,
-                    ratio: 0,
-                    lastCacheAccess: null
-                };
-            }
-        }
+module.exports.createServer = function (config) {
+    config.isSSL = (!config.hasOwnProperty('isSSL') ? false : true)
+    config.auth = (!config.hasOwnProperty('auth') ? false : true)
+    if (!config.hasOwnProperty('path')) {
+        config.path = Config('NoobHTTP').path['public'];
     }
-
+    
+    if (!config.hasOwnProperty('port')) {
+        config.port = (config.isSSL ? 443 : 80);
+    }
+    
+    return new NoobHTTP(config);
 }
